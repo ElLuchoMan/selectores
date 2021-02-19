f97e90f5ae52b76a33a635932196445dd127814c
ElLuchoMan <luchoman96@gmail.com>
2021-02-18 19:44:30 -0500

    Creada pagina selector

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./paises/paises.module').then(m => m.PaisesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent {
+
+  constructor() { }
+
+
+}
